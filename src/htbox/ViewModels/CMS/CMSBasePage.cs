3046d9fb2d7fce7acc8972e622592d1a34d1c591
Tony Surma <tony@surmafamily.com>
2013-08-11 13:01:45 -0500

    better nav

--- src/htbox/ViewModels/CMS/CMSBasePage.cs
+++ src/htbox/ViewModels/CMS/CMSBasePage.cs
@@ -6,14 +6,25 @@ using N2.Details;
 namespace htbox.ViewModels.CMS {
     [WithEditableName, WithEditableTitle]
     public abstract class CMSBasePage : ContentItem {
-        
+        [EditableText("Menu Link", 90, HelpText = "Text entered here is used instead of the Title in Menus and Links.")]
+        public virtual string AlternateMenuTitle { get; set; }
+
+        public string MenuLink {
+            get {
+                if (!String.IsNullOrWhiteSpace(AlternateMenuTitle)) {
+                    return AlternateMenuTitle;
+                }
+                return Title;
+            }
+        }
+
+        [EditableFreeTextArea("Page Content", 100)]
+        public virtual string Text { get; set; }
+
         [EditableCheckBox("Show In Menus", 110, DefaultValue = true)]
         public override bool Visible {
             get { return base.Visible; }
             set { base.Visible = value; }
         }
-
-        [EditableFreeTextArea]
-        public virtual string Text { get; set; }
     }
 }
