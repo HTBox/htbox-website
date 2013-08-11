5b55c470bf988fc8240a5a3d4bd498bbc72829c4
Tony Surma <tony@surmafamily.com>
2013-08-11 17:21:09 -0500

    make homepage more editable

--- src/htbox/ViewModels/CMS/HomePage.cs
+++ src/htbox/ViewModels/CMS/HomePage.cs
@@ -14,8 +14,23 @@ namespace htbox.ViewModels.CMS {
     [RestrictParents(typeof(IRootPage))]
     public class HomePage : CMSBasePage {
 
+        [EditableText("First Sub Heading", 200)]
+        public virtual string SubHeading1 { get; set; }
 
+        [EditableText("First Sub Body", 210)]
+        public virtual string SubBody1 { get; set; }
 
+        [EditableText("Second Sub Heading", 230)]
+        public virtual string SubHeading2 { get; set; }
+
+        [EditableText("Second Sub Body", 240)]
+        public virtual string SubBody2 { get; set; }
+
+        [EditableText("Third Sub Heading", 260)]
+        public virtual string SubHeading3 { get; set; }
+
+        [EditableText("Third Sub Body", 270)]
+        public virtual string SubBody3 { get; set; }
 
         [EditableText("Footer Text (for every page)", 600)]
         public virtual string FooterText { get; set; }
