d9d70eb7622b0c88012231e9c65903922b145e2b
Tony Surma <tony@surmafamily.com>
2013-09-02 17:37:59 -0500

    implemented mailchimp
    tested with personal testing api and list keys for mailchimp

--- src/htbox/Controllers/MailingListController.cs
+++ src/htbox/Controllers/MailingListController.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+using MailChimp;
+using MailChimp.Types;
 
 using htbox.ViewModels;
 using htbox._ImportedCode.Benthos.N2CMS;
 
 namespace htbox.Controllers
 {
-    public class MailingListController : N2CMSAwareMvcController
-    {
+    public class MailingListController : N2CMSAwareMvcController {
+
         public ActionResult Subscribe() {
             //TODO: Move this back to base class
             if (TempData["ViewData"] != null)
@@ -29,10 +33,6 @@ namespace htbox.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Subscribe(MailingListSubscribeViewModel viewModel) {
             //TODO: Remove this as it is for fake processing to check that only ".com" addresses are valid
-            if (!viewModel.EmailAddress.EndsWith(".com")) {
-                ModelState.AddModelError("EmailAddress", "Email Address must be a '.com' address.");
-                TempData["alert-danger"] = "Could not sign up! Please try again.";
-            }
             if (!ModelState.IsValid) {
                 //TODO: move to base class and need to pull in my tempdata cookieprovider so we are multi-server tolerant with tempdata
                 TempData["ViewData"] = ViewData;
@@ -40,6 +40,21 @@ namespace htbox.Controllers
             }
 
             //TODO: do actual subscription processing
+
+            string mailChimpApiKey = ConfigurationSettings.AppSettings["MailChimpApiKey"];
+            string mailChimpListId = ConfigurationSettings.AppSettings["MailChimpListId"];
+
+            var mailChimp = new MCApi(mailChimpApiKey, true);
+
+            mailChimp.ListSubscribe(mailChimpListId, viewModel.EmailAddress,
+                                    new List.Merges {
+                                        {"FNAME", viewModel.FirstName},
+                                        {"LNAME", viewModel.LastName}
+                                    }, new List.SubscribeOptions() {
+                                        DoubleOptIn = false,
+                                        SendWelcome = false,
+                                        UpdateExisting = true
+                                    });
 
             //TODO: move to base class and need to pull in my tempdata cookieprovider so we are multi-server tolerant with tempdata
             TempData["alert-success"] = "Thank you! You are now signed up.";
